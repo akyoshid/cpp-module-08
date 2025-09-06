799a26f2ca95cf1783f847e149b51d081c925603
akyoshid <akyoshid@student.42tokyo.jp>
2025-09-06 17:54:58 +0000

    [ADD] main function

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,8 +6,22 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 17:51:13 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/09/06 17:52:19 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/09/06 17:54:31 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Span.hpp"
+
+int main()
+{
+    Span sp = Span(5);
+    sp.addNumber(6);
+    sp.addNumber(3);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    sp.addNumber(11);
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
+    return 0;
+}
